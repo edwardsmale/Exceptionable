6db79a7605376cb32af0f4adb3f5e7a50bbc57b4
Ed Smale <smaleedward@gmail.com>
2022-01-07 09:28:10 +0000

    Initial commit

---
+++ Exceptionable/Exceptionable.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace EAS.Exceptionable
+{
+    public class Exceptionable<T>
+    {
+        public readonly T Value;
+        public readonly bool HasValue;
+        public readonly Exception Exception;
+
+        /// <summary>
+        /// Initialises an Exceptionable object with a value of type <typeparamref name="T"/>.
+        /// </summary>
+        public Exceptionable(T value)
+        {
+            this.HasValue = true;
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Initialises an Exceptionable object with an exception.
+        /// </summary>
+        public Exceptionable(Exception ex)
+        {
+            this.Exception = ex;
+        }
+
+        // The true and false operators and the boolean cast operator allow an Exceptionable<T>
+        // to be used in a conditional, e.g.
+        //
+        //     var response = this.GetResponse();  // Returns Exceptionable<ResponseObject>
+        //
+        //     if (response)
+        //     {
+        //         ...
+        //     }
+        //
+        //     if (!response)
+        //     {
+        //         ...
+        //     }
+        public static bool operator true(Exceptionable<T> m) => m.HasValue;
+        public static bool operator false(Exceptionable<T> m) => !m.HasValue;
+        public static implicit operator bool(Exceptionable<T> m) => m.HasValue;
+
+        // Allows an Exceptionable<T> to be passed to a method that takes a T parameter:
+        public static implicit operator T(Exceptionable<T> m)
+        {
+            if (!m.HasValue)
+            {
+                throw new InvalidOperationException("Exceptionable object must have a value");
+            }
+            else
+            {
+                return m.Value;
+            }
+        }
+
+        // Allows a method with return type Exceptionable<T> to return a T:
+        public static implicit operator Exceptionable<T>(T value) => new Exceptionable<T>(value);
+
+        // Allows a method with return type Exceptionable<T> to return an Exception:
+        public static implicit operator Exceptionable<T>(Exception ex) => new Exceptionable<T>(ex);
+
+        // Allows comparison between an Exceptionable<T> and a T.
+        public static bool operator ==(Exceptionable<T> x, T y) => x.HasValue && x.Value.Equals(y);
+        public static bool operator !=(Exceptionable<T> x, T y) => !(x == y);
+
+        // Allows comparison between a T and an Exceptionable<T>.
+        public static bool operator ==(T x, Exceptionable<T> y) => (y == x);
+        public static bool operator !=(T x, Exceptionable<T> y) => (y != x);
+
+        // Allows comparison between two Exceptionable<T> objects by value.
+        public static bool operator ==(Exceptionable<T> x, Exceptionable<T> y) => x.Equals(y);
+        public static bool operator !=(Exceptionable<T> x, Exceptionable<T> y) => !(x == y);
+
+        public override bool Equals(object obj)
+        {
+            return
+                obj is Exceptionable<T> other &&
+                this.HasValue &&
+                other.HasValue &&
+                this.Value.Equals(other.Value);
+        }
+
+        public override int GetHashCode() => this.GetValueOrException().GetHashCode();
+        public override string ToString() => this.GetValueOrException().ToString();
+
+        private object GetValueOrException()
+        {
+            return this.HasValue ? this.Value : (object)this.Exception;
+        }
+    }
+}
---
+++ Exceptionable/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace EAS.Exceptionable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                // A method with return type Exceptionable<T> can return a T:
+
+                var result = MethodThatReturnsExceptionable(42);
+
+                Assert(result == "42 is a great number.");
+            }
+
+            {
+                // A method with return type Exceptionable<T> can return an exception:
+
+                var result = MethodThatReturnsExceptionable(100);
+
+                Assert(result.Exception.GetType() == typeof(ArgumentException));
+            }
+
+            {
+                // An Exceptionable<T> can be tested for a value like this:
+
+                var result = MethodThatReturnsExceptionable(100);
+
+                if (result)
+                {
+                    Console.WriteLine("The result has a value.");
+                }
+
+                if (!result)
+                {
+                    Console.WriteLine("The result has no value.");
+                }
+
+                var flag = false;
+
+                if (flag && result)
+                {
+                    Console.WriteLine("The flag is true and the result has a value.");
+                }
+
+                if (!flag || !result)
+                {
+                    Console.WriteLine("Either the flag is false or the result has no value.");
+                }
+            }
+
+            {
+                var exceptionable = new Exceptionable<int>(3);
+
+                // An Exceptionable<T> can be compared to a T for equality:
+
+                Assert(exceptionable == 3);
+                Assert(3 == exceptionable);
+            }
+
+            {
+                // An Exceptionable<T> can be assigned a T value:
+
+                Exceptionable<int> exceptionable = 99;
+
+                Assert(exceptionable == 99);
+            }
+
+            {
+                // An Exceptionable<T> can be passed to a method that expects a T:
+
+                var exceptionable = new Exceptionable<string>("A piece of text");
+
+                var result = MethodThatTakesAString(exceptionable);
+
+                Assert(result == "A piece of text was passed.");
+            }
+
+            {
+                // A non-valued exceptionable throws an exception when implicitly converted to T:
+
+                var exceptionable = new Exceptionable<string>(new Exception("Went wrong!!! :("));
+
+                try
+                {
+                    MethodThatTakesAString(exceptionable); // Will throw
+                }
+                catch (Exception ex)
+                {
+                    Assert(ex.GetType() == typeof(InvalidOperationException));
+                }
+            }
+
+            void Assert(bool condition)
+            {
+                if (!condition)
+                {
+                    throw new Exception("Assert failed.");
+                }
+            }
+        }
+
+        public static string MethodThatTakesAString(string str)
+        {
+            return str + " was passed.";
+        }
+
+        public static Exceptionable<string> MethodThatReturnsExceptionable(int number)
+        {
+            if (number <= 42)
+            {
+                return number + " is a great number.";
+            }
+            else
+            {
+                return new ArgumentException();
+            }
+        }
+    }
+}
